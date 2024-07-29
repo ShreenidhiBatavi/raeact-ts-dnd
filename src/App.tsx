faf3a02271468fb9e05419e9889b2b78e869ae3d
Shreenidhi Batavi <81751036+ShreenidhiBatavi@users.noreply.github.com>
2024-07-29 21:25:52 +0530

    adding component logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useEffect, useState } from "react";
+import TaskCard from "./components/TaskCard";
+import { Task, statuses } from "./utils/task";
+import data from "./data.json";
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const columns = statuses.map((s) => {
+    const taskInColumn = tasks.filter((t) => t.status === s);
+    return {
+      status: s,
+      tasks: taskInColumn,
+    };
+  });
+
+  useEffect(() => {
+    setTasks(data.tasks);
+  }, []);
 
   return (
-    <div>
-      <h1>shri Haraye Namaha</h1>
+    <div className="flex divide-x">
+      {columns.map((column) => (
+        <div>
+          <h1>{column.status}</h1>
+          {column.tasks.map((task) => (
+            <TaskCard task={task} />
+          ))}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
